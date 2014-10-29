a09ef0ee2343f724fff3a41387953ed25015932c
Laurynas Ragauskas <laurynas.ragauskas@gmail.com>
2014-10-29 11:42:21 +0200

    fixed hybrid auth flow that was blocked by bad params access from post

--- lib/omniauth/strategies/google_oauth2.rb
+++ lib/omniauth/strategies/google_oauth2.rb
@@ -67,11 +67,12 @@ module OmniAuth
       end
 
       def custom_build_access_token
-        if request.xhr? && request.params['code']
-          verifier = request.params['code']
+        r_body = JSON.parse( request.body.read ) if request.xhr?
+        if request.xhr? && r_body["code"]
+          verifier = r_body["code"]
           client.auth_code.get_token(verifier, { :redirect_uri => 'postmessage'}.merge(token_params.to_hash(:symbolize_keys => true)),
                                      deep_symbolize(options.auth_token_params || {}))
-        elsif verify_token(request.params['id_token'], request.params['access_token'])
+        elsif verify_token(r_body['id_token'], r_body['access_token'])
           ::OAuth2::AccessToken.from_hash(client, request.params.dup)
         else
           orig_build_access_token
